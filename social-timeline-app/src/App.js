7731c82716b6919274df59ce78dd1126de7d9087
Maxaplays <maxapinos@gmail.com>
2023-08-18 14:28:29 -0500

    added Profile pictures and timestamps for posts

--- social-timeline-app/src/App.js
+++ social-timeline-app/src/App.js
@@ -4,17 +4,79 @@ import Timeline from "./components/Timeline"; // Import the Timeline component
 
 function App() {
   // Sample data for posts
+  const currentTime = new Date("2023-08-18T14:17:00");
+
   const posts = [
-    { id: 1, content: "Hello, world!", author: "User123" },
-    { id: 2, content: "This is a sample post.", author: "JohnDoe" },
-    { id: 3, content: "Another post here!", author: "JaneSmith" },
-    { id: 4, content: "Greetings from React!", author: "ReactFan" },
-    { id: 5, content: "Just sharing a thought...", author: "ThoughtfulPerson" },
-    { id: 6, content: "Checking out this app!", author: "AppExplorer" },
-    { id: 7, content: "Coding all day!", author: "CodeNinja" },
-    { id: 8, content: "The weather is nice today.", author: "WeatherWatcher" },
-    { id: 9, content: "Sharing my favorite recipe!", author: "Foodie" },
-    { id: 10, content: "Learning React is fun!", author: "LearningEnthusiast" },
+    {
+      id: 1,
+      content: "Woof, world!",
+      author: "Buddy",
+      avatar: require("./assets/pfp1.jpg"),
+      timestamp: currentTime,
+    },
+    {
+      id: 2,
+      content: "Just taking a paw-some stroll in the park.",
+      author: "Charlie",
+      avatar: require("./assets/pfp2.jpg"),
+      timestamp: new Date(currentTime.getTime() - 5 * 60 * 1000), // 5 minutes ago
+    },
+    {
+      id: 3,
+      content: "Bark bark bark!",
+      author: "Luna",
+      avatar: require("./assets/pfp3.jpg"),
+      timestamp: new Date(currentTime.getTime() - 45 * 60 * 1000), // 45 minutes ago
+    },
+    {
+      id: 4,
+      content: "Pawsitively excited about React!",
+      author: "Max",
+      avatar: require("./assets/pfp4.jpg"),
+      timestamp: new Date(currentTime.getTime() - 1.5 * 60 * 60 * 1000), // 1.5 hours ago
+    },
+    {
+      id: 5,
+      content: "Ruff ruff ruff...",
+      author: "Daisy",
+      avatar: require("./assets/pfp5.jpg"),
+      timestamp: new Date(currentTime.getTime() - 4 * 60 * 60 * 1000), // 4 hours ago
+    },
+    {
+      id: 6,
+      content: "Sniffing around this app!",
+      author: "Rocky",
+      avatar: require("./assets/pfp6.jpg"),
+      timestamp: new Date(currentTime.getTime() - 10 * 60 * 60 * 1000), // 10 hours ago
+    },
+    {
+      id: 7,
+      content: "Coding like a fur-ninja!",
+      author: "Coco",
+      avatar: require("./assets/pfp7.jpg"),
+      timestamp: new Date(currentTime.getTime() - 16 * 60 * 60 * 1000), // 16 hours ago
+    },
+    {
+      id: 8,
+      content: "What a pawsome weather today!",
+      author: "Bailey",
+      avatar: require("./assets/pfp8.jpg"),
+      timestamp: new Date(currentTime.getTime() - 1 * 24 * 60 * 60 * 1000), // 1 day ago
+    },
+    {
+      id: 9,
+      content: "Sharing my favorite bone recipe!",
+      author: "Lola",
+      avatar: require("./assets/pfp9.jpg"),
+      timestamp: new Date(currentTime.getTime() - 5 * 24 * 60 * 60 * 1000), // 5 days ago
+    },
+    {
+      id: 10,
+      content: "Learning React is a tail-wagging experience!",
+      author: "Bentley",
+      avatar: require("./assets/pfp10.jpg"),
+      timestamp: new Date(currentTime.getTime() - 14 * 24 * 60 * 60 * 1000), // 14 days ago
+    },
   ];
 
   return (
--- social-timeline-app/src/components/Post.js
+++ social-timeline-app/src/components/Post.js
@@ -1,17 +1,53 @@
 import React from "react";
-import LikeButton from "./LikeButton"; // Import the LikeButton component
+import LikeButton from "./LikeButton";
 import "../styles/Post.css";
 
+function calculateTimeSincePost(timestamp) {
+  const currentTime = new Date();
+  const timeDifference = currentTime - timestamp;
+
+  const msInSecond = 1000;
+  const msInMinute = msInSecond * 60;
+  const msInHour = msInMinute * 60;
+  const msInDay = msInHour * 24;
+  const msInWeek = msInDay * 7;
+  const msInYear = msInDay * 365;
+
+  if (timeDifference < msInMinute) {
+    return `${Math.floor(timeDifference / msInSecond)}s`;
+  } else if (timeDifference < msInHour) {
+    return `${Math.floor(timeDifference / msInMinute)}m`;
+  } else if (timeDifference < msInDay) {
+    return `${Math.floor(timeDifference / msInHour)}h`;
+  } else if (timeDifference < msInWeek) {
+    return `${Math.floor(timeDifference / msInDay)}d`;
+  } else if (timeDifference < msInYear) {
+    return `${Math.floor(timeDifference / msInWeek)}w`;
+  } else {
+    return `${Math.floor(timeDifference / msInYear)}y`;
+  }
+}
+
 function Post({ post }) {
+  const timeSincePost = calculateTimeSincePost(post.timestamp);
+
   return (
     <li className="post">
       <div className="post-content">
+        <div className="post-content-header">
+          <img
+            src={post.avatar}
+            alt={`Profile of ${post.author}`}
+            className="avatar"
+          />
+          <small>{post.author}</small>
+          <span class="dot"></span>
+          <span className="timestamp">{timeSincePost}</span>
+        </div>
         <p>{post.content}</p>
-        <small>{post.author}</small>
       </div>
       <div className="post-actions">
         <LikeButton />
-        {/* Add more actions like "Reply" button */}
       </div>
     </li>
   );
